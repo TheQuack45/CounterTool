eaffe3e3401c1bccc57e07ea52ec88d058f3c793
Daniel Quackenbush <TheQuack45@gmail.com>
2017-11-15 13:02:42 -0500

    Added basic Counter class as part of application Model.

---
+++ CounterTool/CounterTool/Model/Counter.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CounterTool.Model
+{
+    public class Counter
+    {
+        #region Members definition
+        #region Fields definition
+        private int _value;
+        #endregion Fields definition
+
+        #region Properties definition
+        public string Name { get; set; }
+
+        public int Value
+        {
+            get { return this._value; }
+            set { this.SetValue(value); }
+        }
+        public int Step { get; set; }
+        public int Maximum { get; set; }
+        public int Minimum { get; set; }
+        #endregion Properties definition
+        #endregion Members definition
+
+        #region Constructors definition
+        public Counter(string name)
+        {
+            this.Name = name;
+        }
+        #endregion Constructors definition
+
+        #region Methods definition
+        public void Increment()
+        {
+            this.Value += this.Step;
+        }
+
+        public void Decrement()
+        {
+            this.Value -= this.Step;
+        }
+
+        private void SetValue(int value)
+        {
+            if (this._value + value > this.Maximum)
+                { this._value = this.Maximum; }
+            else if (this._value + value < this.Minimum)
+                { this._value = this.Minimum; }
+            else
+                { this._value += value; }
+        }
+        #endregion Methods definition
+    }
+}
